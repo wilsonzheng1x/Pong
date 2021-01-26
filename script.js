0d68cc36c29597a19ee7ed624bec08bbfc799640
Wilson Zheng <wzheng536@gmail.com>
2021-01-25 23:33:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,33 @@ var render = function() {
   context.fillStyle = "powderblue";
   context.fillRect(0, 0, width, height);
 };
+
+function Paddle(x, y, width, height) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.x_speed = 0;
+  this.y_speed = 0;
+};
+
+Paddle.prototype.render = function() {
+ context.fillStyle = "red";
+ context.fillRect(this.x, this.y, this.width, this.height);
+}
+
+function Player() {
+   this.paddle = new Paddle(175, 580, 50, 10);
+}
+
+function Computer() {
+  this.paddle = new Paddle(175, 10, 50, 10);
+}
+
+Player.prototype.render = function() {
+  this.paddle.render();
+};
+
+Computer.prototype.render = function() {
+  this.paddle.render();
+};
